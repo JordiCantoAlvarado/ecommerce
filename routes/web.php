480f49f58baf04005acc343188b9dcda902faef5
JordiCantoAlvarado <jordicantoalvarado@hotmail.com>
2026-04-07 20:09:31 +0200

    Controlador: Admin + Ruta + Middleware

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')
+->middleware('auth');
